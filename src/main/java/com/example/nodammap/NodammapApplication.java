a9eda5187d8f87f3e82bc73951711eca9184d85b
fortune-man <wnguddl96@knou.ac.kr>
2024-07-01 12:26:15 +0900

    Chore: querydsl 환경설정 테스트

---
+++ src/main/java/com/example/nodammap/NodammapApplication.java
@@ -0,0 +1,13 @@
+package com.example.nodammap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodammapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodammapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nodammap/NodammapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nodammap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodammapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
